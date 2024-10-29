8af820937945959f5b5f90720f176de5af04ec20
artxmix <artem.artusevskij01@gmail.com>
2024-10-29 11:41:33 +0300

    Added a base class for commands

---
+++ ProjectManagementStudio.ViewModel/Command/RelayCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace ProjectManagementStudio.ViewModel.Command;
+
+public class RelayCommand(Action execute) : ICommand
+{
+    private readonly Action _execute = execute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter)
+    {
+        _execute.Invoke();
+    }
+}
