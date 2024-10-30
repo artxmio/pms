0b3ac262417a77760bcf1bcde6e3261bc8e6dae8
DRANIK20073 <2007gruntov@gmail.com>
2024-10-30 23:32:23 +0300

    Повтор прошлого коммита

--- ProjectManagementStudio.View/MainWindow/MainWindow.xaml.cs
+++ ProjectManagementStudio.View/MainWindow/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace ProjectManagementStudio.View.MainWindow;
+﻿using ProjectManagementStudio.View.MainWindow.Pages;
+using System.Windows; 
+
+namespace ProjectManagementStudio.View.MainWindow;
 
 public partial class MainWindow : IMainWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        RegistrationFrame.Navigate(new RegistrationPage());
     }
 }
