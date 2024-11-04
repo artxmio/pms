fabad8c19b817cb9fd1356800cd5ed8cef69ecd8
artxmix <artem.artusevskij01@gmail.com>
2024-11-04 09:29:31 +0300

    Исправил ошибки с валидацией почты

--- ProjectManagementStudio.ViewModel/ValidationsRules/EmailValidationRules.cs
+++ ProjectManagementStudio.ViewModel/ValidationsRules/EmailValidationRules.cs
@@ -6,7 +6,7 @@ namespace ProjectManagementStudio.ViewModel.ValidationsRules;
 
 public class EmailValidationRules : ValidationRule
 {
-    private readonly Regex _regex = new("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{5,50}$");
+    private readonly Regex _regex = new("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,50}$");
 
     public override ValidationResult Validate(object value, CultureInfo cultureInfo)
     {
