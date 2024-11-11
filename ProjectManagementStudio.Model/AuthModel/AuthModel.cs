27f862c72a3c5867f28a87a6688380e537e59e85
artxmix <artem.artusevskij01@gmail.com>
2024-11-11 13:12:47 +0300

    Добавил модель для окон авторизации и регистрации

---
+++ ProjectManagementStudio.Model/AuthModel/AuthModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProjectManagementStudio.Model.AuthModel;
+
+public class AuthModel : IAuthModel, INotifyPropertyChanged
+{
+    private string _login = "";
+    private string _password = "";
+    private string _email = "";
+
+    public string Login
+    {
+        get => _login;
+        set
+        {
+            _login = value;
+            OnPropertyChanged();
+        }
+    }
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            OnPropertyChanged();
+        }
+    }
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            _email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ ProjectManagementStudio.Model/AuthModel/IAuthModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectManagementStudio.Model.AuthModel;
+
+public interface IAuthModel
+{
+    string Login { get; set; }
+    string Password { get; set; }
+    string Email { get; set; }
+}
