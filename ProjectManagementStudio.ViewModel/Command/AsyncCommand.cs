3e7d4d11996e73b7c32ad50f452f79385f5ec112
artxmix <artem.artusevskij01@gmail.com>
2024-11-11 12:55:42 +0300

    Добавил AsyncCommand

---
+++ ProjectManagementStudio.ViewModel/Command/AsyncCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace ProjectManagementStudio.ViewModel.Command;
+
+public class AsyncCommand : ICommand
+{
+    private readonly Func<Task> _execute;
+
+    public AsyncCommand(Func<Task> execute)
+    {
+        _execute = execute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public async void Execute(object? parameter)
+    {
+        await _execute.Invoke();
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
