ff90e8c8e301b4ec33660a06b0ab3cedc6a23e74
artxmix <artem.artusevskij01@gmail.com>
2024-11-15 15:09:45 +0300

    Добавил BaseModel и RegisterModel

--- ProjectManagementStudio.Model/AuthModel/AuthModel.cs
+++ ProjectManagementStudio.Model/AuthModel/AuthModel.cs
@@ -1,21 +1,13 @@
 ﻿using ProjectManagementStudio.Model.UserSavedData.Wrapper;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace ProjectManagementStudio.Model.AuthModel;
 
-public class AuthModel : IAuthModel, INotifyPropertyChanged
+public class AuthModel : BaseModel.BaseModel, IAuthModel
 {
-    private readonly Regex _loginRegex = new("^[a-zA-Z0-9_]{6,20}$");
-    private readonly Regex _passwordRegex = new("^[a-zA-Z0-9@#$%&*()<>[\\]{}]{6,24}$");
-
     private readonly IUserDataMementoWrapper _wrapper;
 
-    private string _email = "";
-    private bool _isValid = false;
-
-    public string login
+    public override string login
     {
         get => _wrapper.UserLogin;
         set
@@ -25,22 +17,12 @@ public class AuthModel : IAuthModel, INotifyPropertyChanged
             Validate();
         }
     }
-    public string password
+    public override string password
     {
         get => _wrapper.UserPassword;
         set
         {
             _wrapper.UserPassword = value;
-            OnPropertyChanged();
-            Validate();
-        }
-    }
-    public string email
-    {
-        get => _email;
-        set
-        {
-            _email = value;
             OnPropertyChanged();
             Validate();
         }
@@ -71,18 +53,11 @@ public class AuthModel : IAuthModel, INotifyPropertyChanged
         _wrapper = wrapper;
     }
 
-    private void Validate()
+    protected override void Validate()
     {
         if (_loginRegex.IsMatch(login) && _passwordRegex.IsMatch(password))
             IsValid = true;
         else
             IsValid = false;
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- ProjectManagementStudio.Model/AuthModel/IAuthModel.cs
+++ ProjectManagementStudio.Model/AuthModel/IAuthModel.cs
@@ -4,5 +4,4 @@ public interface IAuthModel
 {
     string login { get; set; }
     string password { get; set; }
-    string email { get; set; }
 }
