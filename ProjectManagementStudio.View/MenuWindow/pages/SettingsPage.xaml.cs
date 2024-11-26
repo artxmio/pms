4bd79b017a14d7854c03552b9535025b4e8082c0
DRANIK20073 <2007gruntov@gmail.com>
2024-11-26 17:34:19 +0300

    Исправил размеры ProfilePage, теперь текст во всплывающих подсказсказ отображается по центру, изменил выпадающие списки(надо найти как сделать закругленными или что-то другое) и checkbox в settings.

--- ProjectManagementStudio.View/MenuWindow/pages/SettingsPage.xaml.cs
+++ ProjectManagementStudio.View/MenuWindow/pages/SettingsPage.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace ProjectManagementStudio.View.MenuWindow.Pages;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ProjectManagementStudio.View.MenuWindow.Pages;
 
 public partial class SettingsPage : ISettingsPage
 {
     public SettingsPage()
     {
         InitializeComponent();
+
     }
 }
+
+
+
+
 
