8286d84a9909ee73b5e6dc39585963546492c1e7
artxmix <artem.artusevskij01@gmail.com>
2025-06-02 14:32:04 +0300

    save

---
+++ ProjectManagementStudio.Model/ResponseModels/CreateTaskResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace ProjectManagementStudio.Model.ResponseModels;
+
+[DataContract]
+public class CreateTaskResponse
+{
+    [DataMember(Name = "success")]
+    public bool Success { get; set; }
+
+    [DataMember(Name = "message")]
+    public string Message { get; set; }
+
+    [DataMember(Name = "tag_id")]
+    public int TaskId { get; set; }
+
+    public CreateTaskResponse()
+    {
+        Success = false;
+        Message = " ";
+        TaskId = -1;
+    }
+}
